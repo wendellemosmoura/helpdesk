d31baa36e6c8698468b29b6d2ecf0bf054a2f03c
wendel.lemos <wendel_lemos_cg@hotmail.com>
2023-07-13 08:24:32 -0300

    findById para chamado

---
+++ src/main/java/com/wendel/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,25 @@
+package com.wendel.helpdesk.resources;
+
+import com.wendel.helpdesk.domain.Chamado;
+import com.wendel.helpdesk.domain.dtos.ChamadoDTO;
+import com.wendel.helpdesk.services.ChamadoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/chamados")
+public class ChamadoResource {
+
+    @Autowired
+    private ChamadoService chamadoService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
+        Chamado obj = chamadoService.findById(id);
+        return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+}
---
+++ src/main/java/com/wendel/helpdesk/services/ChamadoService.java
@@ -0,0 +1,21 @@
+package com.wendel.helpdesk.services;
+
+import com.wendel.helpdesk.domain.Chamado;
+import com.wendel.helpdesk.repositories.ChamadoRepository;
+import com.wendel.helpdesk.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ChamadoService {
+
+    @Autowired
+    private ChamadoRepository chamadoRepository;
+
+    public Chamado findById(Integer id) {
+        Optional<Chamado> obj = chamadoRepository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado" + id));
+    }
+}
