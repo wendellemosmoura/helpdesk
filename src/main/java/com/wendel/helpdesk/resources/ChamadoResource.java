c5512cadecc76ce16fc29a01cafe762231f70bcc
wendel.lemos <wendel_lemos_cg@hotmail.com>
2023-07-13 09:02:53 -0300

    findAll para chamado

--- src/main/java/com/wendel/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/wendel/helpdesk/resources/ChamadoResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/v1/chamados")
 public class ChamadoResource {
@@ -21,5 +24,12 @@ public class ChamadoResource {
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id) {
         Chamado obj = chamadoService.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll() {
+        List<Chamado> list = chamadoService.findAll();
+        List<ChamadoDTO> listDTO = list.stream().map(ChamadoDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 }
--- src/main/java/com/wendel/helpdesk/services/ChamadoService.java
+++ src/main/java/com/wendel/helpdesk/services/ChamadoService.java
@@ -6,6 +6,7 @@ import com.wendel.helpdesk.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -16,6 +17,10 @@ public class ChamadoService {
 
     public Chamado findById(Integer id) {
         Optional<Chamado> obj = chamadoRepository.findById(id);
-        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado" + id));
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado ID: " + id));
+    }
+
+    public List<Chamado> findAll() {
+        return chamadoRepository.findAll();
     }
 }
